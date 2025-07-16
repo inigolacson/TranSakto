135cb39f09bb5fba0d8d0c2d42e4d882573b7da9
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-07-16 14:50:21 +0800

    feat: create welcome screen without gradient background

--- mobile/app/auth/index.tsx
+++ mobile/app/auth/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { Link } from 'expo-router';
 
 export default function WelcomeScreen() {
     return (
-        <View className='flex-1 justify-around items-center px-8 py-16'> 
-            <View className='w-48 h-48 bg-white rounded-lg shadow-md justify-center items-center'>
-                <Text>Logo or Artwork</Text>
+       
+        <View className='flex-1 justify-center items-center px-8 py-16'> 
+            {/* logo or artwork section */}
+            <View className='w-80 h-80 bg-white rounded-lg shadow-md justify-center items-center mb-20'>
+                <Text className='text-grey-400 text-center text-sm'>Logo or Artwork</Text>
+            </View>
+            {/* welcome message */}
+            <View className='items-center mb-11'>
+                <Text className='text-4xl font-bold text-gray-800 mb-4'>Welcome</Text>
+                <Text className='text-lg text-center text-gray-600 px-4'>
+                    A smarter POS-easy to use, built to match your unique business needs.
+                </Text>
+            </View>
+            {/* start button (redirects to login) */}
+            <View className='w-full items-center'>
+                <Link href="/auth/login" asChild>
+                    <TouchableOpacity className='bg-mainBlue py-4 px-8 rounded-full shadow-md w-3/4 items-center'>
+                        <Text className='text-white text-xl font-semibold'>Start Today!</Text>
+                    </TouchableOpacity>
+                </Link>
+
             </View>
         </View>
     )
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainBlue: '#6FB3E3',
+      }
+    },
   },
   plugins: [],
 }
