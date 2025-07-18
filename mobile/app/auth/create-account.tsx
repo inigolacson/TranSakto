5a1fe620229af9645244ab1033a3d53ffa71ae15
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-07-18 14:58:48 +0800

    feat: add name textfield

--- mobile/app/auth/create-account.tsx
+++ mobile/app/auth/create-account.tsx
@@ -6,6 +6,8 @@ import { FontAwesome } from '@expo/vector-icons';
 const bgImage = require('../../assets/images/background/index.webp')
 
 export default function CreateAccount() {
+
+    const [isNameFocused, setIsNameFocused] = useState(false);
     const [isEmailFocused, setIsEmailFocused] = useState(false);
     const [isConfirmPasswordFocused, setIsConfirmPasswordFocused] = useState(false);
     const [isPasswordFocused, setIsPasswordFocused] = useState(false);
@@ -14,6 +16,7 @@ export default function CreateAccount() {
     const [showConfirmPassword, setShowConfirmPassword] = useState(false);
     const [checked, setChecked] = useState(false)
 
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [emailConfirm, setEmailConfirm] = useState('');
     const [password, setPassword] = useState('');
@@ -25,9 +28,22 @@ export default function CreateAccount() {
             <View className='items-center mb-8'>
                 <Text className='text-4xl font-extrabold text-headerColor mb-4 font-inter tracking-widest'>Create Account</Text> 
             </View>
+            {/* name */}
+            <View className=' bg-textBoxWhite rounded-full py-2 px-8 w-3/4 mb-5 flex-row items-center space-x-10'>
+                <FontAwesome name='user' size={20} color="#C44422" className='mr-3'/>
+                <TextInput
+                value={name}
+                onChangeText={setName}
+                onFocus={() => setIsNameFocused(true)}
+                onBlur= {() => setIsNameFocused(false)}
+                placeholder= {isNameFocused ? '' : 'name'}
+                className='flex-1 text-tempBlack pr-2 font-ron'
+                placeholderTextColor='#3A3A3A'
+                />
+            </View>
             {/* email */}
             <View className='bg-textBoxWhite rounded-full py-2 px-8 mb-5 w-3/4 flex-row items-center space-x-10'>
-                <FontAwesome name='user' size={20} color="#C44422" className='mr-3'/>
+                <FontAwesome name='envelope' size={15} color="#C44422" className='mr-3'/>
                 <TextInput
                 value={email}
                 onChangeText={setEmail}
@@ -40,7 +56,7 @@ export default function CreateAccount() {
             </View>
 
             {/* password */}
-            <View className='bg-textBoxWhite rounded-full py-2 px-8 mb-4 w-3/4 flex-row items-center space-x-10'>
+            <View className='bg-textBoxWhite rounded-full py-2 px-8 mb-5 w-3/4 flex-row items-center space-x-10'>
                 <FontAwesome name='lock' size={20} color="#C44422" className='mr-3'/>
                 <TextInput
                 value={password}
@@ -97,6 +113,16 @@ export default function CreateAccount() {
             <TouchableOpacity className='bg-buttonOrange py-4 px-8 rounded-full shadow-md w-3/4 items-center mb-12'>
                 <Text className='text-white text-xl font-semibold tracking-widest'>Sign Up!</Text>
             </TouchableOpacity>
+
+            {/* divider */}
+                
+            {/* google button */}
+            <TouchableOpacity>
+
+            </TouchableOpacity>
+            {/* facebook button */}
+            <TouchableOpacity>
+                </TouchableOpacity>   
             {/* redirect login */}
             <View className='flex-row items-center'>
                 <Text className='text-subheaderColor font-ron'>Already managing a business?  </Text>
