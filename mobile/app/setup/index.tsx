6088f593086f9090f9801aad924b3d89f7c77f67
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-07-20 13:38:09 +0800

    chore: setup app routing and onboarding files

---
+++ mobile/app/setup/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function OnboardingOne() {
+  return (
+    <View className="flex-1 justify-center items-center w-full h-full bg-tempBlack">
+      <Text className="font-ron-bold text-textBoxWhite text-4xl">Hello </Text>
+    </View>
+  );
+}
---
+++ mobile/app/setup/onboarding-two.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function OnboardingTwo() {
+  return (
+    <View className="flex-1 items-center justify-center w-full h-full bg-tempBlack">
+      <Text className="text-textBoxWhite font-ron text-4xl "> Hello </Text>
+    </View>
+  );
+}
