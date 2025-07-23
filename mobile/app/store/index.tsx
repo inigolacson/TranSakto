ad91fb5b7ac535097a14f0378432b39a2413671d
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-07-23 15:07:24 +0800

    chore: redirect from index to intended file instead of handling default routing to index

---
+++ mobile/app/store/index.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+import { router } from "expo-router";
+
+export default function StoreRedirection() {
+  useEffect(() => {
+    router.replace("/store/code-login");
+  }, []);
+
+  return null;
+}
