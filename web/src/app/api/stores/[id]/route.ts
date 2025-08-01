7b3c250282fb277e94c0df9d55793e8dae974698
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-08-01 17:54:11 +0800

    tweak: refactor for build

--- web/src/app/api/stores/[id]/route.ts
+++ web/src/app/api/stores/[id]/route.ts
@@ -1,23 +1,20 @@
 import { NextRequest } from "next/server";
 import { storeController } from "@/controller/storeController";
 
-export async function GET(
-  req: NextRequest,
-  context: { params: { id: string } }
-) {
+interface RouteContext {
+  params: {
+    id: string;
+  };
+}
+
+export async function GET(req: NextRequest, context: RouteContext) {
   return storeController.getStore(req, context.params.id);
 }
 
-export async function PUT(
-  req: NextRequest,
-  context: { params: { id: string } }
-) {
+export async function PUT(req: NextRequest, context: RouteContext) {
   return storeController.updateStore(req, context.params.id);
 }
 
-export async function DELETE(
-  req: NextRequest,
-  context: { params: { id: string } }
-) {
+export async function DELETE(req: NextRequest, context: RouteContext) {
   return storeController.deleteStore(req, context.params.id);
 }
